d5cedbcce1fa47179d0c5ba226d9e4a7769f7597
Siva Lopez <siva.lopez@gmail.com>
2020-07-19 22:54:07 +1000

    Added my environment details.

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'coffee-silo.au', // the auth0 domain prefix
+    audience: 'coffee-silo', // the audience set for the auth0 app
+    clientId: 'sfuFrJY2mOQ0wULfuxc9TXkTM0hwbA0r', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
